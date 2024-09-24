fb2e9bbdaebdc56ea0ba2a4b854ddded001e2667
Luis Reis <145137414+LuisReis09@users.noreply.github.com>
2024-09-24 17:30:24 -0300

    Update main.py

--- formais/back-end/main.py
+++ formais/back-end/main.py
@@ -4,11 +4,26 @@ from flask import Flask, render_template, jsonify, request
 app = Flask(__name__)
 CORS(app)  # Isso permite CORS para todas as rotas
 
+@app.route("/verifyInput", methods=["POST"])
+def verifyInput():
+    # Essa rota eh responsavel por verificar se o input esta no estilo "letra: producao"
+    texto = request.get_json()["entrada"]
+    print(texto)
+    return jsonify({"message": "verifyInput"})
+
 @app.route("/verifyProduction", methods=["POST"])
 def verifyProduction():
+    # Essa rota deve ser responsavel por retornar se a producao e valida, e adicionar ao back essa producao
     data = request.get_json()
     print(data)
-    return jsonify({"message": "Hello, World!"})
+    return jsonify({"message": "verifyProduction"})
+
+@app.route("/removeProduction", methods=["POST"])
+def removeProduction():
+    # Essa rota deve ser responsavel por retornar se a producao ja esta inserida, e remover do back essa producao
+    data = request.get_json()
+    print(data)
+    return jsonify({"message": "removeProduction"})
 
 
 
